496575049a1c7165269c390144d0edb6058d435c
Hamza el Amri <hamzaelamri2003@gmail.com>
2022-12-07 14:22:44 +0100

    geen goeie

--- Project/resources/views/Poi.blade.php
+++ Project/resources/views/Poi.blade.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="poi.css">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
 <h1>{{$poi_list->Naam}}</h1>
 
 <a href="../Pois/{{$poi_list->Locatie_ID}}">Pois</a>
